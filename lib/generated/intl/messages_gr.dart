debdff38353a22aea76bc56754251825500b76a8
ASUS <noor.kh.mansour@gmail.com>
2021-03-22 15:29:55 +0200

    commit

---
+++ lib/generated/intl/messages_gr.dart
@@ -0,0 +1,59 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a gr locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'gr';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "Abandon" : MessageLookupByLibrary.simpleMessage("Abandon"),
+    "About" : MessageLookupByLibrary.simpleMessage(" über"),
+    "AlertDialogContent" : MessageLookupByLibrary.simpleMessage("Möchten Sie das Formular wirklich verlassen? Alle Änderungen gehen verloren."),
+    "AppBarCodeScreen" : MessageLookupByLibrary.simpleMessage("Code Screen"),
+    "AppBarLogInScreen" : MessageLookupByLibrary.simpleMessage("Anmelden"),
+    "AppBarSignUpScreen" : MessageLookupByLibrary.simpleMessage("Anmelden"),
+    "Arabic" : MessageLookupByLibrary.simpleMessage("Arabisch"),
+    "Cancel" : MessageLookupByLibrary.simpleMessage("Abbrechen"),
+    "ChangeLanguage" : MessageLookupByLibrary.simpleMessage("Sprache ändern"),
+    "ChangePasswordLabelText" : MessageLookupByLibrary.simpleMessage("Passwort ändern"),
+    "ConfirmPasswordLabelText" : MessageLookupByLibrary.simpleMessage("Passwort bestätigen"),
+    "DoNotHaveAnAccount" : MessageLookupByLibrary.simpleMessage("Sie haben noch kein Konto?"),
+    "English" : MessageLookupByLibrary.simpleMessage("Englisch"),
+    "FlutterStore" : MessageLookupByLibrary.simpleMessage("Flutter Store"),
+    "ForgotYourPassword" : MessageLookupByLibrary.simpleMessage("Passwort vergessen?"),
+    "German" : MessageLookupByLibrary.simpleMessage("Deutsch"),
+    "HaveAnAccount" : MessageLookupByLibrary.simpleMessage("Haben Sie ein Konto?"),
+    "LogOut" : MessageLookupByLibrary.simpleMessage("Abmelden"),
+    "MainPage" : MessageLookupByLibrary.simpleMessage("Produkte nach Kategorien"),
+    "MyActivities" : MessageLookupByLibrary.simpleMessage("Meine Aktivitäten"),
+    "MyCart" : MessageLookupByLibrary.simpleMessage("Mein Einkaufswagen"),
+    "MyPurchases" : MessageLookupByLibrary.simpleMessage("Meine Einkäufe"),
+    "NewPasswordLabelText" : MessageLookupByLibrary.simpleMessage("Neues Passwort"),
+    "OldPassword" : MessageLookupByLibrary.simpleMessage("Altes Passwort"),
+    "PasswordLabelText" : MessageLookupByLibrary.simpleMessage("Passwort"),
+    "Setting" : MessageLookupByLibrary.simpleMessage("Einstellungen"),
+    "ShareThisApp" : MessageLookupByLibrary.simpleMessage("Diese App teilen"),
+    "SnackBarLogInScreen" : MessageLookupByLibrary.simpleMessage("Daten verarbeiten"),
+    "SubmitScreen" : MessageLookupByLibrary.simpleMessage("Submit"),
+    "UserNameLabelText" : MessageLookupByLibrary.simpleMessage("Benutzername"),
+    "Version" : MessageLookupByLibrary.simpleMessage("Version"),
+    "buttonTitleSignIn" : MessageLookupByLibrary.simpleMessage("Anmelden"),
+    "buttonTitleSignUp" : MessageLookupByLibrary.simpleMessage("Anmelden"),
+    "phoneNumberLabel" : MessageLookupByLibrary.simpleMessage("Telefonnummer")
+  };
+}
